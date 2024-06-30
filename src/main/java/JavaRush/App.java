9de498ad964815e194db457725371f5f0415e9de
Adm Syd <adm_syd@icloud.com>
2024-06-30 05:34:18 +0500

    configured the Compiler Plugin (Compiling Source Using graalvm-jdk-21.0.3)

---
+++ src/main/java/JavaRush/App.java
@@ -0,0 +1,17 @@
+package JavaRush;
+
+import lombok.Data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    String name;
+    int age;
+    public static void main( String[] args )
+    {
+        System.out.println("H");
+    }
+}
